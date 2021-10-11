02e78852a35ede2cc8a31ec7235fdd67e401fd15
pietabrood <pieter_nortje@outlook.com>
2021-10-11 12:47:26 +0200

    Development: added routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3001
+const port = 3000
 const MongoClient = require('mongodb').MongoClient
 const { ObjectId } = require('mongodb');
 const dotenv = require('dotenv');
